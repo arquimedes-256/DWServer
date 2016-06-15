452c1abacc471c5ac09572dfdc0d96aa1c638259
Lucas Oliveira e Silva <lucas.silva@GHD.local>
2016-06-15 14:13:25 -0300

    interfacefix-relatorioelaina

--- saiku-server/tomcat/webapps/combustivel/controllers/Liberacao.js
+++ saiku-server/tomcat/webapps/combustivel/controllers/Liberacao.js
@@ -4,7 +4,7 @@ var config = {
 	"Habilitar desbloqueio": true, //**(marcar como "true" ou "false" )
 									//se marcar como false, ele ignora as definicoes de desbloqueio
 	
-	"Bloquear a partir do mes":3, //significa que todo mes >= n estara bloqueado, 
+	"Bloquear a partir do mes":7, //significa que todo mes >= n estara bloqueado, 
 								  //ou seja, se você escreve n=3, entao de marco para frente estara BLOQUEADO
 	//"Forcar abrir dia":"01-02-2016" //**(marcar como null ou "dd-mm-yyyy")
 								   //forca abrir uma determinada data para todas as filiais,  
@@ -12,16 +12,14 @@ var config = {
 
 //definicoes de desbloqueio
 var exceptions = {
-  // '1':{dtInicio:new Date(new Date("2016-02-15")-1),dtFim:new Date("2016-02-28")},//VIT
-  // '2':{dtInicio:new Date(new Date("2016-02-28")-1),dtFim:new Date("2016-02-29")},//rj1
- //  '9':{dtInicio:new Date(new Date("2016-02-28")-1),dtFim:new Date("2016-02-29")},//SEP
- //  '3':{dtInicio:new Date(new Date("2016-02-28")-1),dtFim:new Date("2016-02-29")},//STR
- // '6':{dtInicio:new Date(new Date("2016-02-18")-1),dtFim:new Date("2016-02-29")},//SDR
-
- //  '7':{dtInicio:new Date(new Date("2016-02-28")-1),dtFim:new Date("2016-02-29")},//MAC
-   
- //  '5':{dtInicio:new Date(new Date("2016-02-28")-1),dtFim:new Date("2016-02-29")},//PNG
- //  '8':{dtInicio:new Date(new Date("2016-02-28")-1),dtFim:new Date("2016-02-29"),"Forcar abrir dia":"01-02-2016"},//RGD
+   	'1':{dtInicio:new Date(new Date("2016-06-01")-1),dtFim:new Date("2016-06-30")},//VIT
+   	'2':{dtInicio:new Date(new Date("2016-06-01")-1),dtFim:new Date("2016-06-30")},//rj1
+   	'9':{dtInicio:new Date(new Date("2016-06-01")-1),dtFim:new Date("2016-06-30")},//SEP
+   	'3':{dtInicio:new Date(new Date("2016-06-01")-1),dtFim:new Date("2016-06-30")},//STR
+  	'6':{dtInicio:new Date(new Date("2016-06-01")-1),dtFim:new Date("2016-06-30")},//SDR
+   	'7':{dtInicio:new Date(new Date("2016-06-01")-1),dtFim:new Date("2016-06-30")},//MAC
+   	'5':{dtInicio:new Date(new Date("2016-06-01")-1),dtFim:new Date("2016-06-30")},//PNG
+   	'8':{dtInicio:new Date(new Date("2016-06-01")-1),dtFim:new Date("2016-06-30")},//RGD ,"Forcar abrir dia":"01-02-2016"
 };
 if(!config["Habilitar desbloqueio"])
 	exceptions = {};
